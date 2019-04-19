8c3095502bd031f737fc57603627a4024305806e
iamvee <naeini.v@gmail.com>
2019-04-19 13:56:22 +0330

    review and a few facts about collections !

---
+++ S02/collections/what_is_list.py
@@ -0,0 +1,10 @@
+my_list = [10, "hello", 10.2]
+
+print("second item : ", my_list[1])
+print("list --> ", my_list)
+
+
+# print , type , len
+length_of_my_list = len(my_list)
+print("len of list = ", length_of_my_list)
+
---
+++ S02/variables/review_variables.py
@@ -0,0 +1,11 @@
+num = 1
+val = 100
+
+new_var = num + val
+print(new_var)
+
+text = "hello"
+print(text)
+
+text_new = text * 10
+print(text_new)
