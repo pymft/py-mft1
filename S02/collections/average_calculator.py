1ca11d86109c6d9e078ed7b40a9664097058ed33
iamvee <naeini.v@gmail.com>
2019-04-19 14:15:26 +0330

    max, min and some other stuff about lists

--- S02/collections/average_calculator.py
+++ S02/collections/average_calculator.py
@@ -1,2 +1,8 @@
-numbers = [1, 2, 3, 4, 5, 6, 7, 8]
+numbers = [1, 2, 3, -10, 4, 5, 6, 7, 8]
 # sum , len
+sum_vals = sum(numbers)
+length = len(numbers)
+average = sum_vals / length
+print("average of numbers in list", numbers, " is equal to :", average)
+print("and the maximum value is : ", max(numbers))
+print("and the minimum value is : ", min(numbers))
